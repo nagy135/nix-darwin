860bbb6061dddc892a0c6e9291aa550f81055ea1
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-03-27 16:04:00 +0100

    feat: lsp

--- nixvim/lsp.nix
+++ nixvim/lsp.nix
@@ -1,4 +1,41 @@
 {
+  keymaps = [
+    {
+      action = "<cmd>lua vim.lsp.buf.definition()<CR>";
+      key = "gd";
+      options = {
+	silent = true;
+      };
+    }
+    {
+      action = "<cmd>lua vim.lsp.buf.hover()<CR>";
+      key = "K";
+      options = {
+	silent = true;
+      };
+    }
+    {
+      action = "<cmd>lua vim.lsp.buf.rename()<CR>";
+      key = "<leader>rn";
+      options = {
+	silent = true;
+      };
+    }
+    {
+      action = "<cmd>lua vim.diagnostic.goto_prev()<CR>";
+      key = "[d";
+      options = {
+	silent = true;
+      };
+    }
+    {
+      action = "<cmd>lua vim.diagnostic.goto_next()<CR>";
+      key = "]d";
+      options = {
+	silent = true;
+      };
+    }
+  ];
   plugins.lsp = {
     enable = true;
 
