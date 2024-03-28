82fc13f237e6ea56d734a304c880eeea06fd117b
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-03-28 16:09:49 +0100

    refactor: move config to default.nix

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -1,6 +1,5 @@
 { nixvim, lib, ... }: 
 let 
-  nixvimConfiguration = import ./config.nix;
   recursiveMerge = with lib; attrList:
     let f = attrPath:
       zipAttrsWith (n: values:
@@ -13,6 +12,123 @@ let
 	else last values
       );
     in f [] attrList;
+  nixvimConfiguration = {
+    plugins = {
+      treesitter.enable = true;
+      noice.enable = true;
+      comment.enable = true;
+      undotree.enable = true;
+      gitsigns.enable = true;
+      emmet.enable = true;
+      todo-comments.enable = true;
+    };
+      # {
+      #
+      #   "<C-Space>" = "cmp.mapping.complete()";
+      # }
+      # {
+      #   "<C-j>" = "cmp.mapping.scroll_docs(4)";
+      # }
+      # {
+      #   "<C-k>" = "cmp.mapping.scroll_docs(-4)";
+      # }
+    colorschemes.catppuccin.enable = true;
+
+    globals = {
+      mapleader = " ";
+    };
+    options = {
+      number = true; # Show line numbers
+      relativenumber = true; # Show relative line numbers
+      shiftwidth = 2; # Tab width should be 2
+      tabstop = 2; # Tab width should be 2
+      laststatus = 3; # Single line status bar
+    };
+    keymaps = [
+      {
+	key = "<C-h>";
+	action = "<cmd>nohl<CR>";
+      }
+      {
+	key = "<C-c>";
+	action = "<cmd>cclose<CR>";
+      }
+      {
+	key = "<leader>bb";
+	action = "<cmd>b#<CR>";
+      }
+    ];
+
+
+    extraConfigLua = ''
+
+  vim.cmd("set undodir=~/.vim/undodir");
+  vim.cmd("set undofile");
+
+  local cmp = require'cmp'
+
+    cmp.setup({
+      snippet = {
+	-- REQUIRED - you must specify a snippet engine
+	expand = function(args)
+	  vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+	  -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+	  -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+	  -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+	  -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+	end,
+      },
+      window = {
+	-- completion = cmp.config.window.bordered(),
+	-- documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+	['<C-b>'] = cmp.mapping.scroll_docs(-4),
+	['<C-f>'] = cmp.mapping.scroll_docs(4),
+	['<C-Space>'] = cmp.mapping.complete(),
+	['<C-e>'] = cmp.mapping.abort(),
+	['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+	{ name = 'nvim_lsp' },
+	{ name = 'vsnip' }, -- For vsnip users.
+	-- { name = 'luasnip' }, -- For luasnip users.
+	-- { name = 'ultisnips' }, -- For ultisnips users.
+	-- { name = 'snippy' }, -- For snippy users.
+      }, {
+	{ name = 'buffer' },
+      })
+    })
+
+    -- Set configuration for specific filetype.
+    cmp.setup.filetype('gitcommit', {
+      sources = cmp.config.sources({
+	{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+      }, {
+	{ name = 'buffer' },
+      })
+    })
+
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+	{ name = 'buffer' }
+      }
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+	{ name = 'path' }
+      }, {
+	{ name = 'cmdline' }
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false }
+    })
+    '';
+};
 in nixvim.legacyPackages."aarch64-darwin".makeNixvim 
   (
     recursiveMerge
@@ -24,5 +140,8 @@ in nixvim.legacyPackages."aarch64-darwin".makeNixvim
       (import ./lsp.nix)
       (import ./neotree.nix)
       (import ./harpoon.nix)
+      (import ./copilot.nix)
+      (import ./lualine.nix)
+      (import ./flash.nix)
     ]
   )
