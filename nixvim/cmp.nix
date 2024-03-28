623d1d4ebd75e696c91a7c48c6a3276bf737bc24
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-03-28 16:09:39 +0100

    feat: cmp

--- nixvim/cmp.nix
+++ nixvim/cmp.nix
@@ -8,4 +8,38 @@
     cmp-path.enable = true;
     cmp-cmdline.enable = true;
   };
+
+  # TODO: verify that this works
+  keymaps = [
+  {
+    key = "<C-n>";
+    action = ''
+      function(fallback)
+      if cmp.visible() then
+	cmp.select_next_item()
+	  elseif require("luasnip").expand_or_jumpable() then
+	  require("luasnip").expand_or_jump()
+      else
+	fallback()
+	  end
+	  end
+	  '';
+    mode = [ "i" "s" ];
+  }
+  {
+    key = "<C-p>";
+    action = ''
+      function(fallback)
+      if cmp.visible() then
+	cmp.select_prev_item()
+	  elseif require("luasnip").expand_or_jumpable() then
+	  require("luasnip").expand_or_jump()
+      else
+	fallback()
+	  end
+	  end
+	  '';
+    mode = [ "i" "s" ];
+  }
+  ];
 }
