16f5ece2304fba744b2dfb8bc79ba8a67e39e163
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-03-29 00:49:54 +0100

    refactor: separated lf module

---
+++ home/lf.nix
@@ -0,0 +1,43 @@
+	{ pkgs, ... }: {
+		programs.lf = {
+			enable = true;
+			commands = {
+				editor-open = ''$$EDITOR $f'';
+				mkdir = ''
+		''${{
+			printf "Directory Name: "
+				read DIR
+				mkdir $DIR
+		}}
+				'';
+			};
+			keybindings = {
+
+				"\\\"" = "";
+				o = "";
+				c = "mkdir";
+				"." = "set hidden!";
+				"`" = "mark-load";
+				"\\'" = "mark-load";
+				"<enter>" = "open";
+
+				"g~" = "cd";
+				gh = "cd";
+				"gd" = "cd ~/Downloads";
+				"gv" = "cd ~/Videos";
+
+				ee = "editor-open";
+				V = ''''$${pkgs.bat}/bin/bat --paging=always --theme=gruvbox "$f"'';
+
+	# ...
+			};
+
+			settings = {
+				preview = true;
+				hidden = true;
+				drawbox = true;
+				icons = true;
+				ignorecase = true;
+			};
+		};
+	}
