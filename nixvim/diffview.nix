76507be77976ac7f9e1473bc613a4d7d9be69683
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-01 23:49:44 +0200

    feat(nixvim): diffview close binds with q, which-key registration

--- nixvim/diffview.nix
+++ nixvim/diffview.nix
@@ -1,12 +1,32 @@
-{
-  plugins.diffview.enable = true;
-  keymaps = [
-    {
-      key = "<leader>gdh";
-      action = "<cmd>DiffviewFileHistory %<cr>";
-      options = {
-        desc = "file history (diffview)";
+let binds = [
+  {
+    key = "q";
+    mode = "n";
+    action = "<cmd>DiffviewClose<cr>";
+    description = "close diffview";
+  }
+];
+in
+  {
+    plugins.diffview = {
+      enable = true;
+      keymaps = {
+        view = binds;
+        fileHistoryPanel = binds;
       };
-    }
-  ];
-}
+    };
+    plugins.which-key.registrations = {
+      "<leader>gd" = "diff";
+    };
+
+    # global
+    keymaps = [
+      {
+        key = "<leader>gdh";
+        action = "<cmd>DiffviewFileHistory %<cr>";
+        options = {
+          desc = "file history (diffview)";
+        };
+      }
+    ];
+  }
