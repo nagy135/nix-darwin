dde9ea8180cecff1f84e4ffba7874f87123f7c5c
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-03 17:23:21 +0200

    feat(nixvim): blame line keybind

--- nixvim/gitsigns.nix
+++ nixvim/gitsigns.nix
@@ -7,11 +7,14 @@
         vim.api.nvim_buf_set_keymap(bufnr, 'n', ']h', '<cmd>Gitsigns next_hunk<CR>', {})
         vim.api.nvim_buf_set_keymap(bufnr, 'n', '[h', '<cmd>Gitsigns prev_hunk<CR>', {})
 
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ghs', '<cmd>lua require"gitsigns".stage_hunk()<CR>', {})
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ghr', '<cmd>lua require"gitsigns".reset_hunk()<CR>', {})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ghs', '<cmd>lua require"gitsigns".stage_hunk()<CR>', {})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ghr', '<cmd>lua require"gitsigns".reset_hunk()<CR>', {})
 
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ghS', '<cmd>lua require"gitsigns".stage_buffer()<CR>', {})
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ghR', '<cmd>lua require"gitsigns".reset_buffer()<CR>', {})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ghl', '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>', {})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ghb', '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>', {})
+
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ghS', '<cmd>lua require"gitsigns".stage_buffer()<CR>', {})
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ghR', '<cmd>lua require"gitsigns".reset_buffer()<CR>', {})
       end
   '';
   };
