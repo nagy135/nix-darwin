7917a0264c9e465581504c19b1a73d2f3f61d22a
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-05 00:17:03 +0200

    feat(nixvim): action.__raw for harpoon

--- nixvim/harpoon.nix
+++ nixvim/harpoon.nix
@@ -8,26 +8,27 @@
   };
   keymaps = [
     {
-      action = ''<cmd>lua require('harpoon.mark').add_file()<CR>'';
+      action.__raw = ''function() require('harpoon.mark').add_file() end'';
       key = "<leader>ha";
       options = {
         desc = "Add file";
       };
     }
     {
-      action = ''<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>'';
+      action.__raw = ''function() require('harpoon.ui').toggle_quick_menu() end'';
       key = "<leader>hh";
       options = {
         desc = "Toggle quick menu";
       };
     }
     {
-      action = ''<cmd>lua local index = vim.fn.input("Harpoon: ")
-    if index == nil or index == "" then
-        return
-    end
-    require('harpoon.ui').nav_file(tonumber(index))<CR>
-      '';
+      action.__raw = ''function()
+        local index = vim.fn.input("Harpoon: ")
+        if index == nil or index == "" then
+            return
+        end
+        require('harpoon.ui').nav_file(tonumber(index))
+      end'';
       key = "<leader>hi";
       options = {
         desc = "Select with input";
