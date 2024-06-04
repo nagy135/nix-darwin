46eb5bcd03c27793b8c79e7baf35e7e66fd3eb4b
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-05 00:16:48 +0200

    feat(nixvim): other binds copilotchat

--- nixvim/copilot.nix
+++ nixvim/copilot.nix
@@ -3,19 +3,51 @@
   keymaps = [
     {
       key = "<leader>cc";
-      mode = ["n" "v" "i" "x" "s" "o"];
+      mode = ["n" "v" "x" "s" "o"];
       options = {
-        desc = "Copilot chat open";
+        desc = "CopilotChat open";
       };
       action = "<cmd>CopilotChat<cr>";
     }
-    # {
-    #   key = "<leader>cp";
-    #   mode = ["n" "v" "i" "x" "s" "o"];
-    #   options = {
-    #     desc = "Copilot chat prompt";
-    #   };
-    #   action = ''<cmd>lua vim.ui.input({ prompt = 'Enter prompt: ' }, function(input) vim.api.nvim_command("CopilotChat " .. input) end)<cr> '';
-    # }
+    {
+      key = "<leader>cf";
+      mode = ["n" "v" "x" "s" "o"];
+      options = {
+        desc = "CopilotChat fix";
+      };
+      action = "<cmd>CopilotChatFix<cr>";
+    }
+    {
+      key = "<leader>co";
+      mode = ["n" "v" "x" "s" "o"];
+      options = {
+        desc = "CopilotChat optimize";
+      };
+      action = "<cmd>CopilotChatOptimize<cr>";
+    }
+    {
+      key = "<leader>ct";
+      mode = ["n" "v" "x" "s" "o"];
+      options = {
+        desc = "CopilotChat test";
+      };
+      action = "<cmd>CopilotChatTests<cr>";
+    }
+    {
+      key = "<leader>cr";
+      mode = ["n" "v" "x" "s" "o"];
+      options = {
+        desc = "CopilotChat reset";
+      };
+      action = "<cmd>CopilotChatReset<cr>";
+    }
+    {
+      key = "<leader>cp";
+      mode = ["n" "v" "x" "s" "o"];
+      options = {
+        desc = "CopilotChat prompt";
+      };
+      action.__raw = ''function() vim.ui.input({ prompt = 'Enter prompt: ' }, function(input) vim.api.nvim_command("CopilotChat " .. input) end) end'';
+    }
   ];
 }
