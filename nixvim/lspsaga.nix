c24731cff8622c2cd52505e44e7e315177761107
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-05 12:42:49 +0200

    feat(nixvim): lspsaga call hierarchy binds

--- nixvim/lspsaga.nix
+++ nixvim/lspsaga.nix
@@ -3,6 +3,14 @@
     enable = true;
     lightbulb.enable = false;
     symbolInWinbar.enable = false;
+
+    callhierarchy.keys = {
+      edit =["o" "<cr>"];
+      split = "s";
+      vsplit = "v";
+      tabe = "t";
+
+    };
   };
   keymaps = [
     {
@@ -11,7 +19,15 @@
     }
     {
       action = ''<cmd>Lspsaga outline<CR>'';
+      key = "<leader>ll";
+    }
+    {
+      action = ''<cmd>Lspsaga outgoing_calls<CR>'';
       key = "<leader>lo";
+    }
+    {
+      action = ''<cmd>Lspsaga incoming_calls<CR>'';
+      key = "<leader>li";
     }
   ];
 }
