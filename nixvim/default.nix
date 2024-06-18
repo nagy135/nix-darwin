13c86d239e183a1d9ce66cc7caea0fd5d5c5eb77
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-18 15:30:37 +0200

    fix(nixvim): deprecated options field

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -40,7 +40,7 @@ let
     globals = {
       mapleader = " ";
     };
-    options = {
+    opts = {
       number = true; # Show line numbers
       relativenumber = true; # Show relative line numbers
       shiftwidth = 2; # Tab width should be 2
