840d92b3191d6290194ee8fd59f61a7c4ec80843
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-07-12 21:14:43 +0200

    feat(spacebar): init

--- flake.nix
+++ flake.nix
@@ -62,6 +62,7 @@
             htop
             fzf
             git
+            spacebar
             arduino-cli
             nmap
             # python
@@ -131,6 +132,36 @@
           '';
 
           enableTCPIP = true;
+        };
+
+        services.spacebar.enable = true;
+        services.spacebar.package = pkgs.spacebar;
+        services.spacebar.config = {
+          position                   = "top";
+          display                    = "main";
+          height                     = 32;
+          title                      = "off";
+          clock                      = "on";
+          power                      = "on";
+          padding_left               = 20;
+          padding_right              = 20;
+          spacing_left               = 25;
+          spacing_right              = 15;
+          text_font                  = ''"Mononoki Nerd Font:Regular:12.0"'';
+          icon_font                  = ''"Mononoki Nerd Font:Regular:12.0"'';
+          background_color           = "0xff0b0b0b";
+          foreground_color           = "0xffa8a8a8";
+          power_icon_color           = "0xffcd950c";
+          battery_icon_color         = "0xffd75f5f";
+          dnd_icon_color             = "0xffa8a8a8";
+          clock_icon_color           = "0xffa8a8a8";
+          power_icon_strip           = " ";
+          space_icon_color           = "0xffe78a4e";
+          space_icon_color_secondary = "0xff78c4d4";
+          space_icon_color_tertiary  = "0xfffff9b0";
+          clock_icon                 = "📅";
+          dnd_icon                   = "";
+          clock_format               = ''"%d/%m/%y  %R"'';
         };
 
         # Direct log output to $XDG_DATA_HOME/postgresql for debugging.
