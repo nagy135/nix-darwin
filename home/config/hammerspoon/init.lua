fd77a3036fc2e62a7f42db21f4837cb1bb304c04
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-25 17:25:51 +0200

    feat(hammerspoon): new arrow keys

--- home/config/hammerspoon/init.lua
+++ home/config/hammerspoon/init.lua
@@ -2,3 +2,15 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
 	hs.alert.show("Hello World!")
 end)
 
+
+local binds = {
+	{ from_mod = { "cmd", "alt" }, from_key = "h", to_mod = nil, to_key = "left" },
+	{ from_mod = { "cmd", "alt" }, from_key = "j", to_mod = nil, to_key = "down" },
+	{ from_mod = { "cmd", "alt" }, from_key = "k", to_mod = nil, to_key = "up" },
+	{ from_mod = { "cmd", "alt" }, from_key = "l", to_mod = nil, to_key = "right" },
+}
+for _, bind in ipairs(binds) do
+	hs.hotkey.bind(bind.from_mod, bind.from_key, function()
+		hs.eventtap.keyStroke(bind.to_mod, bind.to_key, 0)
+	end)
+end
