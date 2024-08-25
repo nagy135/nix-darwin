fb7e379663bcc2c426a1dc7e2c5a4e9ddb7d8253
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-08-25 17:23:00 +0200

    feat(nixvim): telescope qflist select

--- nixvim/telescope.nix
+++ nixvim/telescope.nix
@@ -10,6 +10,20 @@ in {
   plugins.telescope = {
     extensions.file-browser.enable = true;
     enable = true;
+    settings = {
+      defaults = {
+        mappings = {
+          i = {
+            "<C-q>" = {
+              __raw = "require('telescope.actions').send_selected_to_qflist + require('telescope.actions').open_qflist";
+            };
+            "<C-a>" = {
+              __raw = "require('telescope.actions').send_to_qflist + require('telescope.actions').open_qflist";
+            };
+          };
+        };
+      };
+    };
     keymaps = {
       "gd" = {
         action = "lsp_definitions";
