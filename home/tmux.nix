88d82793eed2e8937ab8ec0a1c035c0d1cb52434
Viktor Nagy <viktor.nagy1995@gmail.com>
2026-03-30 19:48:34 +0200

    feat(tmux): plugins opencode and my fork neolazygit

--- home/tmux.nix
+++ home/tmux.nix
@@ -1,14 +1,37 @@
 { pkgs, ... }:
 let
+  # neolazygit = pkgs.tmuxPlugins.mkTmuxPlugin {
+  #   pluginName = "neolazygit";
+  #   version = "unstable-2026-03-30";
+  #   rtpFilePath = "neolazygit.tmux";
+  #   src = pkgs.fetchFromGitHub {
+  #     owner = "AngryMorrocoy";
+  #     repo = "tmux-neolazygit";
+  #     rev = "5910deee166d6273658533a7302b5be5f0ccc06f";
+  #     hash = "sha256-38Fb3faEjoeHO4k8dP5jxZQ9Oo2PmjN8r07v+UgCAAE=";
+  #   };
+  # };
   neolazygit = pkgs.tmuxPlugins.mkTmuxPlugin {
     pluginName = "neolazygit";
     version = "unstable-2026-03-30";
     rtpFilePath = "neolazygit.tmux";
     src = pkgs.fetchFromGitHub {
-      owner = "AngryMorrocoy";
+      owner = "nagy135";
       repo = "tmux-neolazygit";
-      rev = "5910deee166d6273658533a7302b5be5f0ccc06f";
-      hash = "sha256-38Fb3faEjoeHO4k8dP5jxZQ9Oo2PmjN8r07v+UgCAAE=";
+      rev = "b28b92b8453c50447912d8e240b069e6304c59ca";
+      hash = "sha256-yXSlfH0/rshRMGwzQuOsYX/Xi/3ZHaKlAmObN2w9/Mk=";
+    };
+  };
+
+  opencode = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "opencode";
+    version = "0.0.1";
+    rtpFilePath = "tmux-opencode.tmux";
+    src = pkgs.fetchFromGitHub {
+      owner = "nagy135";
+      repo = "tmux-opencode";
+      rev = "d162005070f7145ffe8f84ca85b07dd8e64b4671";
+      hash = "sha256-+UpN9KREQpoiqc1Q4GoDnfWFvy+wJPX6NuveS+sdcaY=";
     };
   };
 in
@@ -60,6 +83,12 @@ in
         plugin = neolazygit;
         extraConfig = ''
           set -g @open-lazygit 'g'
+        '';
+      }
+      {
+        plugin = opencode;
+        extraConfig = ''
+          set -g @tmux-opencode-window-position '6'
         '';
       }
     ];
