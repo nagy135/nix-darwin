ee5d359872755a95d798a861a4e897909b28a596
Viktor Nagy <viktor.nagy1995@gmail.com>
2026-03-31 21:18:50 +0200

    feat(sesh): extra configs

--- home/sesh.nix
+++ home/sesh.nix
@@ -7,8 +7,42 @@
       session = [
         {
           name = "stuff";
-          path = "$HOME";
+          path = "~";
           startup_command = "fastfetch && ls";
+          windows = [ "neovim" ];
+        }
+        {
+          name = "warehouse";
+          path = "~/Code/warehouse";
+          startup_command = "ls";
+          windows = [ "neovim" "lazygit" ];
+        }
+        {
+          name = "shift-disributor";
+          path = "~/Code/shift-distributor";
+          startup_command = "ls";
+          windows = [ "neovim" "lazygit" ];
+        }
+        {
+          name = "addy-art";
+          path = "~/Code/addy-art-payload";
+          startup_command = "ls";
+          windows = [ "neovim" "lazygit" ];
+        }
+        {
+          name = "nix-darwin";
+          path = "~/Code/nix-darwin";
+          startup_command = "ls";
+        }
+      ];
+      window = [
+        {
+          name = "neovim";
+          startup_script = "nvim .";
+        }
+        {
+          name = "lazygit";
+          startup_script = "lazygit";
         }
       ];
     };
