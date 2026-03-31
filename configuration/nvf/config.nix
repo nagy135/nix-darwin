ecb3687e86f01ce55d97deebe7b666e68e1943b3
Viktor Nagy <viktor.nagy1995@gmail.com>
2026-03-31 23:23:51 +0200

    feat(nvim): debug

--- configuration/nvf/config.nix
+++ configuration/nvf/config.nix
@@ -70,7 +70,32 @@ in {
       isort
       prettierd
       stylua
+      vscode-js-debug
     ];
+
+    debugger.nvim-dap = {
+      enable = true;
+      ui = {
+        enable = true;
+        autoStart = true;
+      };
+      mappings = {
+        continue = "<leader>dc";
+        toggleBreakpoint = "<leader>db";
+        toggleDapUI = "<leader>du";
+        toggleRepl = "<leader>dr";
+        hover = "<leader>dh";
+        restart = "<leader>dR";
+        runLast = "<leader>d.";
+        runToCursor = "<leader>dl";
+        stepInto = "<leader>di";
+        stepOut = "<leader>do";
+        stepOver = "<leader>dn";
+        terminate = "<leader>dt";
+        goDown = "<leader>dj";
+        goUp = "<leader>dk";
+      };
+    };
 
     lsp = {
       enable = true;
@@ -187,6 +212,12 @@ in {
 
       "telescope-file-browser.nvim" = {
         package = pkgs.vimPlugins.telescope-file-browser-nvim;
+      };
+
+      "nvim-dap-vscode-js" = {
+        package = pkgs.vimPlugins.nvim-dap-vscode-js;
+        lazy = false;
+        after = builtins.readFile ./lua/debug-js.lua;
       };
 
       "snacks.nvim" = {
