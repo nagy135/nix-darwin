70cb42e14522bf48e1d96ed976990ce2e4a27617
Viktor Nagy <viktor.nagy1995@gmail.com>
2026-06-07 19:57:46 +0200

    feat: multi-host rewrite

--- flake.nix
+++ flake.nix
@@ -29,11 +29,55 @@
     home-manager,
     ...
   }: let
-    home = "/Users/Viktor";
     platform = "aarch64-darwin";
-    username = "Viktor";
-    hostname = "Viktor-Mac";
+    hosts = {
+      sensory = {
+        username = "Viktor";
+        home = "/Users/Viktor";
+      };
+      personal = {
+        username = "viktornagy";
+        home = "/Users/viktornagy";
+      };
+    };
     pkgs = nixpkgs.legacyPackages.${platform};
+    mkDarwinConfiguration = hostname: host:
+      nix-darwin.lib.darwinSystem {
+        specialArgs = {
+          inherit self;
+          inherit (host) username;
+          userHome = host.home;
+          nvf = inputs.nvf;
+        };
+        modules = [
+          ./configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              backupFileExtension = "save";
+              extraSpecialArgs = {
+                inherit inputs;
+                inherit (host) username;
+                userHome = host.home;
+              };
+              users.${host.username} = import ./home;
+            };
+            users.users.${host.username}.home = host.home;
+          }
+        ];
+      };
+    mkHomeConfiguration = hostname: host: {
+      name = "${host.username}@${hostname}";
+      value = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit inputs;
+          inherit (host) username;
+          userHome = host.home;
+        };
+        modules = [./home];
+      };
+    };
   in {
     formatter.${platform} = pkgs.writeShellApplication {
       name = "nix-fmt";
@@ -43,40 +87,14 @@
       '';
     };
 
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Viktors-MacBook-Pro
-    darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
-      specialArgs = {
-        inherit home self username;
-        nvf = inputs.nvf;
-      };
-      modules = [
-        ./configuration
-        home-manager.darwinModules.home-manager
-        {
-          home-manager = {
-            backupFileExtension = "save";
-            extraSpecialArgs = {
-              inherit inputs;
-            };
-            users.${username} = import ./home;
-          };
-          users.users.${username}.home = home;
-        }
-      ];
-    };
+    # Build darwin flakes using:
+    # $ darwin-rebuild build --flake .#sensory
+    # $ darwin-rebuild build --flake .#personal
+    darwinConfigurations = nixpkgs.lib.mapAttrs mkDarwinConfiguration hosts;
 
-    homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = {
-        inherit inputs;
-      };
-      modules = [./home];
-    };
-
-    homeConfigurations."${username}@${hostname}" = self.homeConfigurations.${username};
+    homeConfigurations = nixpkgs.lib.listToAttrs (nixpkgs.lib.mapAttrsToList mkHomeConfiguration hosts);
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations.${hostname}.pkgs;
+    darwinPackages = self.darwinConfigurations.sensory.pkgs;
   };
 }
